cca69bd9d2186419abff7ad176b7339981104d98
arif99050 <arifulislam99050@gmail.com>
2022-05-09 15:34:37 +0600

    home page extra section

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -3,6 +3,7 @@ import { Carousel } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import Products from "../Products/Products/Products";
 import banner1 from "../../../images/bannerLaptop.jpg";
+import laptop from "../../../images/laptop-with-blank-black-screen-white-table.jpg";
 
 const Home = () => {
   return (
@@ -18,11 +19,31 @@ const Home = () => {
             <h1 className="">
               <span className="text-primary">Welcome to laptop warehouse</span>
             </h1>
-            
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
       <Products></Products>
+      
+
+
+        <div className="row m-5">
+        <h1>Get the Authentic product</h1>
+        <div className="col-lg-7 col-md-7-col-sm-12 ">
+        
+        <h3 className="mt-5">
+            
+            Get the best and authentic laptop from us. We suplly the best
+            product all over bangladesh. we import our product from dubai. We
+            maintain this warehouse since many years. Meet with us and get the best laptop.
+          </h3>
+          
+          
+        </div>
+        <div className="col-lg-5 col-md-7-col-sm-12 d-flex justify-content-center">
+          <img className="img-fluid" src={laptop} alt="" />
+        </div>
+      </div>
+        
       {/* <Link>Manage Inventory</Link> */}
       <Link to="/manageinventory">
         <button className="btn btn-primary w-50 m-5">Manage Inventoy</button>
